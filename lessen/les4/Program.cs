bcb80140d52f5b16e76c51ab4657bc30c0fa3f25
gdmgent-kobecoen <tim.depaepe@arteveldehs.be>
2019-10-24 09:05:18 +0200

    WIP

---
+++ lessen/les4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace les4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // double fahr = 100;
+            // double cels = (fahr - 32)* (5.0/9.0);
+
+            // Console.WriteLine(cels);
+
+            Boolean test;
+            test = true;
+            Console.WriteLine(test.ToString());
+        }
+    }
+}
---
+++ opdrachten/opdracht 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace opdracht_4
+{
+    class Program
+    {
+        static void Add(int getal1, int getal2){
+           Console.WriteLine("Optellen");
+           int result = getal1 + getal2;
+           Console.WriteLine(result);
+       }
+       static void Subtrack(int getal1, int getal2){
+           Console.WriteLine("Aftrekken");
+           int result2 = getal1 - getal2;
+           Console.WriteLine(result2);
+       }
+       static void Multiply(int getal1, int getal2){
+           Console.WriteLine("Vermenigvuldigen");
+           int result3 = getal1 * getal2;
+           Console.WriteLine(result3);
+       }
+       static void Devide(int getal1, int getal2){
+           Console.WriteLine("Delen");
+           double result4 = (double) getal1 / getal2;
+           Console.WriteLine(result4);
+       }
+
+       static void Main(string[] args){
+           Add(8,3);
+           Subtrack(8,3);
+           Multiply(8,3);
+           Devide(8,3);
+       }
+
+    }
+}
